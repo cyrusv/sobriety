4a5f5d42333ef5226c74e8bf50d244951a5c1f7a
Cyrus Vafadari <cyrusv@alum.mit.edu>
2017-03-04 23:17:38 -0500

    Update date onResume instead of onCreate of activity

--- app/src/main/java/com/cyrusv/onedayatatime/CountdownActivity.java
+++ app/src/main/java/com/cyrusv/onedayatatime/CountdownActivity.java
@@ -3,7 +3,6 @@ package com.cyrusv.onedayatatime;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -16,8 +15,8 @@ import java.util.Date;
 public class CountdownActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onResume() {
+        super.onResume();
         setContentView(R.layout.activity_countdown);
 
         long startDate = getSharedPreferences("userSettings", Context.MODE_PRIVATE).getLong(getString(R.string.date_key), 0);
